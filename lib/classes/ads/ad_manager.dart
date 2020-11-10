1b3fc0d359df8fcc8f6ce17431335d15ef882453
wkulczi <killer98001@gmail.com>
2020-11-10 18:35:03 +0100

    uncomment for static ads

--- lib/classes/ads/ad_manager.dart
+++ lib/classes/ads/ad_manager.dart
@@ -2,39 +2,39 @@ import 'dart:io';
 
 class AdManager {
 
-  static String get appId {
-    if (Platform.isAndroid) {
-      return "ca-app-pub-3940256099942544~4354546703";
-    } else if (Platform.isIOS) {
-      return "ca-app-pub-3940256099942544~2594085930";
-    } else {
-      throw new UnsupportedError("Unsupported platform");
-    }
-  }
   // static String get appId {
   //   if (Platform.isAndroid) {
-  //     return "ca-app-pub-7326884795508047~2791709945";
+  //     return "ca-app-pub-3940256099942544~4354546703";
+  //   } else if (Platform.isIOS) {
+  //     return "ca-app-pub-3940256099942544~2594085930";
   //   } else {
   //     throw new UnsupportedError("Unsupported platform");
   //   }
   // }
 
-  static String get bannerAdUnitId {
+  static String get appId {
     if (Platform.isAndroid) {
-      return "ca-app-pub-3940256099942544/8865242552";
-    } else if (Platform.isIOS) {
-      return "ca-app-pub-3940256099942544/4339318960";
+      return "ca-app-pub-7326884795508047~2791709945";
     } else {
       throw new UnsupportedError("Unsupported platform");
     }
   }
 
   // static String get bannerAdUnitId {
-  //   if (Platform.isAndroid) {
-  //     return "ca-app-pub-7326884795508047/9165546609";
-  //   }
-  //   else {
+  //   if (Platform.isAndroid)
+  //   } else if (Platform.isIOS) {
+  //     return "ca-app-pub-3940256099942544/4339318960";
+  //   } else {
   //     throw new UnsupportedError("Unsupported platform");
   //   }
   // }
+  //
+  static String get bannerAdUnitId {
+    if (Platform.isAndroid) {
+      return "ca-app-pub-7326884795508047/9165546609";
+    }
+    else {
+      throw new UnsupportedError("Unsupported platform");
+    }
+  }
 }
